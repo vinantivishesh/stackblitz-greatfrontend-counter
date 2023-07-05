6fd4ac5cb0ccfea0b03784d3c81f8eb987ad7490
vinantivishesh <50644736+vinantivishesh@users.noreply.github.com>
2023-07-05 12:30:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Clicks: {count}
+      </button>
+    </div>
+  );
+}
